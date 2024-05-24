de3665b72fe363acd216a43e96bf9e802b0543a4
Jean Yves Ehouman <yvesehouman09@gmail.com>
2024-05-24 19:10:30 +0900

    Implement providers for supabase

---
+++ providers/userProvider.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { MyUserContextProvider } from "@/hooks/useUser";
+import { FC } from "react";
+interface  UserProviderProps {
+    children: React.ReactNode;
+};
+
+export const UserProvider: FC< UserProviderProps> = ({
+    children
+}) => {
+    return (
+        <MyUserContextProvider>
+            { children }
+        </MyUserContextProvider>
+    );
+}
