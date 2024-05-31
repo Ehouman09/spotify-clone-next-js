6c316fefa8c68eb8c5a79a3ba9f203f32554c00c
Jean Yves Ehouman <yvesehouman09@gmail.com>
2024-05-31 18:32:56 +0900

    Update PlayerContent.tsx

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -5,6 +5,7 @@ import { FC } from "react";
 import { MediaItem } from "./MediaItem";
 import { LikeButton } from "./LikeButton";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
+import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 
 interface PlayerContentProps {
     song: Song;
@@ -40,6 +41,28 @@ export const PlayerContent: FC<PlayerContentProps> = ({
                         className="text-black"
                     />
                 </div>
+            </div>
+
+            <div className="hidden h-full md:flex justify-center w-full max-w-[722px] gap-x-6">
+                <AiFillStepBackward
+                    onClick={ () => {} } 
+                    size={30}
+                    className="text-neutral-400 cursor-pointer hover:text-white transition"
+                />
+                <div 
+                    onClick={ () => {} }
+                    className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer"
+                >
+                    <Icon
+                        size={30}
+                        className="text-black"
+                    />
+                </div>
+                <AiFillStepForward 
+                    onClick={ () => {} }
+                    size={30}
+                    className="text-neutral-400 cursor-pointer hover:text-white transition"
+                />
             </div>
         </div>
     );
